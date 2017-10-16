32813fec5446c561f44b5f64ad78ce3ebba721ac
snotJam <1587436668@qq.com>
2017-10-16 17:31:58 +0800

    add cache util

---
+++ src/main/java/com/aprivate/thinklibrary/utils/GsonUtil.java
@@ -0,0 +1,115 @@
+package com.aprivate.thinklibrary.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Gson工具类
+ * Created by Cxy on 2016/11/15.
+ */
+public class GsonUtil {
+    private static Gson gson = null;
+
+    static {
+        if (gson == null) {
+            gson = new Gson();
+        }
+    }
+
+    private GsonUtil() {
+    }
+
+    /**
+     * 对象转成json
+     *
+     * @param object
+     * @return
+     */
+    public static String GsonString(Object object) {
+        String gsonString = null;
+        if (gson != null) {
+            gsonString = gson.toJson(object);
+        }
+        return gsonString;
+    }
+
+    /**
+     * json转成bean
+     *
+     * @param gsonString
+     * @param cls
+     * @return
+     */
+    public static <T> T GsonToBean(String gsonString, Class<T> cls) {
+        T t = null;
+        if (gson != null) {
+            t = gson.fromJson(gsonString, cls);
+        }
+        return t;
+    }
+
+
+    /**
+     * Json转换为List，避免泛型编译期被擦除
+     *
+     * @param json
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> fromJsonList(String json, Class<T> clazz) {
+        List<T> lst = new ArrayList<T>();
+        try {
+            JsonArray array = new JsonParser().parse(json).getAsJsonArray();
+            for (final JsonElement elem : array) {
+                lst.add(new Gson().fromJson(elem, clazz));
+            }
+        } catch (Exception e) {
+        }
+        return lst;
+    }
+
+
+
+    /**
+     * 转成list中有map的
+     *
+     * @param gsonString
+     * @return
+     */
+    public static <T> List<Map<String, T>> GsonToListMaps(String gsonString) {
+        List<Map<String, T>> list = null;
+        if (gson != null) {
+            list = gson.fromJson(gsonString,
+                    new TypeToken<List<Map<String, T>>>() {
+                    }.getType());
+        }
+        return list;
+    }
+
+    /**
+     * 转成map的
+     *
+     * @param gsonString
+     * @return
+     */
+    public static <T> Map<String, T> GsonToMaps(String gsonString) {
+        Map<String, T> map = null;
+        if (gson != null) {
+            map = gson.fromJson(gsonString, new TypeToken<Map<String, T>>() {
+            }.getType());
+        }
+        return map;
+    }
+
+    public static Gson getGson() {
+        return gson;
+    }
+}
